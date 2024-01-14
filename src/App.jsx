5bc8fbfb64991a811b67ca1c74ee88af734ee75c
Sushil Hemrom <sushil500530@gmail.com>
2024-01-14 20:58:24 +0600

    add basic project setup use tailwind,react with redux

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center my-5 text-fuchsia-600">
+        Hello Redux!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
